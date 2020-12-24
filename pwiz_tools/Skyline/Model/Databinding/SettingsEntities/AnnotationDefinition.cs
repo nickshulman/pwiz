48a635401fd41c5fdf3c3baaa12ce607ab91228b
Nicholas Shulman <nicksh@proteinms.net>
2020-12-24 05:20:18 -0800

    AnnotationDefinition

---
+++ pwiz_tools/Skyline/Model/Databinding/SettingsEntities/AnnotationDefinition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using pwiz.Skyline.Model.Databinding.Entities;
+
+namespace pwiz.Skyline.Model.Databinding.SettingsEntities
+{
+    public class AnnotationDefinition : SkylineObject
+    {
+        public string Name { get; set; }
+        
+    }
+}

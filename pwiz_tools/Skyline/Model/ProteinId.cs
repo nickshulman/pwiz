fc4968bc76777e7daedbc0f68265aacffe727f58
Nicholas Shulman <nicksh@uw.edu>
2022-01-12 16:15:34 -0800

    Protein Grouping

---
+++ pwiz_tools/Skyline/Model/ProteinId.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Web.UI.WebControls;
+using pwiz.Skyline.Model.DocSettings;
+
+namespace pwiz.Skyline.Model
+{
+    public class ProteinId : Identity
+    {
+        public ProteinId(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class ProteinDocNode : DocNode
+    {
+        public ProteinDocNode(ProteinId id) : base(id)
+        {
+        }
+
+        public override AnnotationDef.AnnotationTarget AnnotationTarget
+        {
+            get { throw new InvalidOperationException(); }
+        }
+
+
+    }
+}

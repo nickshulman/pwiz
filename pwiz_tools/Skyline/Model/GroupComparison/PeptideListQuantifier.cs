29ce4f7712cf255169e0479ccfeb69acb025564d
nickshulman <nicksh@uw.edu>
2023-03-14 16:09:59 -0700

    Calibration curves at the protein level

---
+++ pwiz_tools/Skyline/Model/GroupComparison/PeptideListQuantifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using pwiz.Common.Collections;
+
+namespace pwiz.Skyline.Model.GroupComparison
+{
+    public class PeptideListQuantifier
+    {
+        public PeptideListQuantifier(IEnumerable<PeptideQuantifier> peptideQuantifiers)
+        {
+            PeptideQuantifiers = ImmutableList.ValueOf(peptideQuantifiers);
+        }
+
+        public ImmutableList<PeptideQuantifier> PeptideQuantifiers { get; }
+    }
+}

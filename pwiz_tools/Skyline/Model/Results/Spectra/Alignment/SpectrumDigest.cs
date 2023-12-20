a02f41786d100d557011c5c16ac35c2bc367f568
nicksh <nicksh@proteinms.net>
2023-12-20 05:37:17 -0800

    Spectrum Alignment

---
+++ pwiz_tools/Skyline/Model/Results/Spectra/Alignment/SpectrumDigest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using pwiz.Common.Collections;
+
+namespace pwiz.Skyline.Model.Results.Spectra.Alignment
+{
+    public struct SpectrumDigest
+    {
+        private ImmutableList<double> _list;
+        public SpectrumDigest(IEnumerable<double> value)
+        {
+            _list = ImmutableList.ValueOf(value);
+        }
+
+        public ImmutableList<double> Value
+        {
+            get { return _list ?? ImmutableList<double>.EMPTY; }
+        }
+        public int Length
+        {
+            get { return Value.Count; }
+        }
+
+        public double? SimilarityScore(SpectrumDigest other)
+        {
+            if (Length != other.Length)
+            {
+                return null;
+            }
+
+            double sumXX = 0;
+            double sumXY = 0;
+            double sumYY = 0;
+            for (int i = 0; i < Length; i++)
+            {
+                double x = Value[i];
+                double y = other.Value[i];
+                sumXX += x * x;
+                sumXY += x * y;
+                sumYY += y * y;
+            }
+
+            if (sumXX <= 0 || sumYY <= 0)
+            {
+                return null;
+            }
+
+            return sumXY / Math.Sqrt(sumXX * sumYY);
+        }
+
+        public static SpectrumDigest FromSpectrum(int digestLength, double scanWindowLower, double scanWindowUpper,
+            IEnumerable<KeyValuePair<double, double>> mzIntensities)
+        {
+            int binCount = Math.Max(8192, digestLength);
+            var binnedSpectrum = BinnedSpectrum.BinSpectrum(binCount, scanWindowLower, scanWindowUpper, mzIntensities);
+            IList<double> vector = binnedSpectrum.Intensities;
+            while (vector.Count > digestLength)
+            {
+                vector = HaarWaveletTransform(vector);
+            }
+            return new SpectrumDigest(vector);
+        }
+
+        public static double[] HaarWaveletTransform(IList<double> vector)
+        {
+            int n = vector.Count / 2;
+            double[] result = new double[n];
+
+            for (int i = 0; i < n / 2; i++)
+            {
+                // Calculate the average and difference
+                result[i] = (vector[2 * i] + vector[2 * i + 1]) / Math.Sqrt(2.0);
+                result[n / 2 + i] = (vector[2 * i] - vector[2 * i + 1]) / Math.Sqrt(2.0);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ pwiz_tools/Skyline/Model/Results/Spectra/DigestedSpectrumMetadata.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using pwiz.Common.Spectra;
+using pwiz.ProteowizardWrapper;
+using pwiz.Skyline.Model.Results.Spectra.Alignment;
+
+namespace pwiz.Skyline.Model.Results.Spectra
+{
+    public class DigestedSpectrumMetadata
+    {
+        private const int DIGEST_SIZE = 1024;
+        public DigestedSpectrumMetadata(SpectrumMetadata spectrumMetadata, SpectrumDigest digest)
+        {
+            SpectrumMetadata = spectrumMetadata;
+            Digest = digest;
+        }
+
+        public static DigestedSpectrumMetadata FromSpectrum(MsDataSpectrum spectrum)
+        {
+            if (spectrum == null)
+            {
+                return null;
+            }
+
+            return new DigestedSpectrumMetadata(spectrum.Metadata,
+                SpectrumDigest.FromSpectrum(DIGEST_SIZE, );
+        }
+
+        public string Id
+        {
+            get { return SpectrumMetadata.Id; }
+        }
+        public double RetentionTime
+        {
+            get { return SpectrumMetadata.RetentionTime; }
+        }
+        public SpectrumMetadata SpectrumMetadata { get; }
+        public SpectrumDigest Digest { get; }
+        private const double BIN_SIZE = 0.250125;
+
+        public static IList<double> BinSpectrum(IList<double> mzs, IList<double> intensities)
+        {
+            var maxMz = mzs.Append(0).Max();
+            if (maxMz == 0)
+            {
+                return Array.Empty<double>();
+            }
+
+            int maxBinIndex = PowerOfTwoGreaterThan((int)(maxMz / BIN_SIZE));
+            double[] result = new double[maxBinIndex];
+            for (int i = 0; i < mzs.Count; i++)
+            {
+                double mz = mzs[i];
+                if (mz < 0)
+                {
+                    continue;
+                }
+
+                int binNumber = (int)(mz / BIN_SIZE);
+                if (binNumber < 0 || binNumber >= maxBinIndex)
+                {
+                    continue;
+                }
+
+                result[binNumber] += intensities[i];
+            }
+
+            return result;
+        }
+
+        private static int PowerOfTwoGreaterThan(int value)
+        {
+            const int maxValue = 1 << 30;
+            for (int power = 1; power < maxValue; power *= 2)
+            {
+                if (power > value)
+                {
+                    return power;
+                }
+            }
+
+            return maxValue;
+        }
+    }
+}

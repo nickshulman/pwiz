63e53148860cd26cb46dbea47955ffbc537e61d4
Nicholas Shulman <nicksh@proteinms.net>
2024-12-31 19:56:31 -0800

    ExtractImagesFromResources commandline tool

---
+++ pwiz_tools/Skyline/Executables/DevTools/ExtractImagesFromResources/ExtractImagesFromResources/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using CommandLine;
+
+namespace ExtractImagesFromResources
+{
+    internal class Program
+    {
+        private static Dictionary<string, byte[]> ImageHeaders = new Dictionary<string, byte[]>
+        {
+            { "png", new byte[] { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A } },
+            { "jpg", new byte[] { 0xFF, 0xD8, 0xFF } },
+            { "bmp", new byte[] { 0x42, 0x4d } },
+            { "gif", new byte[] { 0x47, 0x49, 0x46, 0x38 } }
+        };
+        public class Options
+        {
+            [Value(0)]
+            public string Path { get; set; }
+        }
+        
+        static int Main(string[] args)
+        {
+            int result = -1;
+            Parser.Default.ParseArguments<Options>(args).WithParsed(options => result = ExtractImages(options))
+                .WithNotParsed(HandleParseError);
+            return result;
+        }
+
+        public static int ExtractImages(Options options)
+        {
+            var path = Path.GetFullPath(options.Path);
+            if (File.Exists(path))
+            {
+                ExtractImagesFromFile(path);
+            }
+            else if (Directory.Exists(path))
+            {
+                ExtractImagesFromFolder(path);
+            }
+            else
+            {
+                Console.Error.WriteLine("{0} does not exist", path);
+                return -1;
+            }
+
+            return 0;
+        }
+
+        public static void ExtractImagesFromFile(string resxPath)
+        {
+            var doc = XDocument.Load(resxPath);
+            bool changed = false;
+            foreach (XElement el in doc.Root.Elements("data"))
+            {
+                if (el.Attribute("type")?.Value != "System.Drawing.Bitmap, System.Drawing")
+                {
+                    continue;
+                }
+
+                if (el.Attribute("mimetype")?.Value != "application/x-microsoft.net.object.bytearray.base64")
+                {
+                    continue;
+                }
+
+                var valueElement = el.Elements("value").FirstOrDefault();
+                if (valueElement == null)
+                {
+                    continue;
+                }
+
+                var bytes = Convert.FromBase64String(valueElement.Value);
+                var extension = GetImageExtension(bytes);
+                if (extension == null)
+                {
+                    continue;
+                }
+
+                var folder = Path.GetDirectoryName(resxPath);
+
+                var filename = el.Attribute("name").Value + "." + extension;
+                var imageFileName = Path.Combine(folder!, filename);
+                File.WriteAllBytes(imageFileName, bytes);
+                valueElement.Value = filename;
+                el.SetAttributeValue("mimetype", null);
+                changed = true;
+            }
+
+            if (changed)
+            {
+                doc.Save(resxPath);
+            }
+        }
+
+        public static void ExtractImagesFromFolder(string path)
+        {
+            foreach (var f in Directory.GetFiles(path))
+            {
+                if (f.EndsWith(".resx", StringComparison.OrdinalIgnoreCase))
+                {
+                    ExtractImagesFromFile(f);
+                }
+            }
+        }
+        static void HandleParseError(IEnumerable<Error> errors)
+        {
+            foreach (var e in errors)
+            {
+                if (e is HelpRequestedError || e is VersionRequestedError)
+                {
+                    continue;
+                }
+                Console.WriteLine($"Error: {e}");
+            }
+        }
+
+        private static string GetImageExtension(byte[] bytes)
+        {
+            foreach (var entry in ImageHeaders)
+            {
+                var header = entry.Value;
+                if (header.SequenceEqual(bytes.Take(header.Length)))
+                {
+                    return entry.Key;
+                }
+            }
+
+            return null;
+        }
+
+
+    }
+}
---
+++ pwiz_tools/Skyline/Executables/DevTools/ExtractImagesFromResources/ExtractImagesFromResources/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ExtractImagesFromResources")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExtractImagesFromResources")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("de8c6ce2-60e6-4320-ae9b-cd845059bc4f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
